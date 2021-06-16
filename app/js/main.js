4b232cf529124d400da4551620e18efe9d82716c
Kulinich Ekaterina <kulinich.ekaterina.andreevna@gmail.com>
2021-06-16 18:11:08 +0300

    wip catalog

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,36 @@
+"use strict";
+
+var rangeSlider = document.getElementById('range-slider');
+
+if (rangeSlider) {
+  noUiSlider.create(rangeSlider, {
+    start: [1850, 25768],
+    connect: true,
+    step: 1,
+    range: {
+      'min': [1850],
+      'max': [25768]
+    }
+  });
+  var input0 = document.getElementById('input-0');
+  var input1 = document.getElementById('input-1');
+  var inputs = [input0, input1];
+  rangeSlider.noUiSlider.on('update', function (values, handle) {
+    inputs[handle].value = Math.round(values[handle]);
+  });
+
+  var setRangeSlider = function setRangeSlider(i, value) {
+    var arr = [null, null];
+    arr[i] = value;
+    rangeSlider.noUiSlider.set(arr);
+  };
+
+  inputs.forEach(function (el, index) {
+    el.addEventListener('change', function (e) {
+      setRangeSlider(index, e.currentTarget.value);
+    });
+  });
+}
 /**
   * название функции
   *
