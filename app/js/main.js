85844ad7104fec692da95fa4fca5f9c6a4ec4151
Kulinich Ekaterina <kulinich.ekaterina.andreevna@gmail.com>
2021-08-27 17:27:36 +0300

    wip

--- app/js/main.js
+++ app/js/main.js
@@ -61,6 +61,331 @@ function init() {
 }
 "use strict";
 
+function _createForOfIteratorHelper(o, allowArrayLike) { var it = typeof Symbol !== "undefined" && o[Symbol.iterator] || o["@@iterator"]; if (!it) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e) { throw _e; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = it.call(o); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e2) { didErr = true; err = _e2; }, f: function f() { try { if (!normalCompletion && it["return"] != null) it["return"](); } finally { if (didErr) throw err; } } }; }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
+var catalogList = document.querySelector('.catalog-list');
+var catalogMore = document.querySelector('.catalog__more');
+var prodModal = document.querySelector('[data-graph-target="prod-modal"] .modal-content');
+var prodModalSlider = prodModal.querySelector('.modal-slider .swiper-wrapper');
+var prodModalPreview = prodModal.querySelector('.modal-slider .modal-preview');
+var prodModalInfo = prodModal.querySelector('.modal-info__wrapper');
+var prodModalDescr = prodModal.querySelector('.modal-prod-descr');
+var prodModalChars = prodModal.querySelector('.prod-chars');
+var prodModalVideo = prodModal.querySelector('.prod-modal__video');
+var prodQuantity = 5;
+var dataLength = null;
+var modal = null;
+
+var normalPrice = function normalPrice(str) {
+  return String(str).replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ');
+};
+
+var prodSlider = new Swiper('.modal-slider__container', {
+  slidesPerView: 1,
+  spaceBetween: 20
+});
+
+if (catalogList) {
+  var loadProducts = function loadProducts() {
+    var quantity = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 5;
+    fetch('../data/data.json').then(function (response) {
+      return response.json();
+    }).then(function (data) {
+      dataLength = data.length;
+      catalogList.innerHTML = '';
+
+      for (var i = 0; i < dataLength; i++) {
+        if (i < quantity) {
+          var item = data[i];
+          console.log(item);
+          catalogList.innerHTML += "\n              <li class=\"catalog-list__item\">\n                <article class=\"product\">\n                  <div class=\"product__image\">\n                    <img src=\"".concat(item.mainImage, "\" alt=\"").concat(item.title, "\">\n                    <div class=\"product__btns\">\n                      <button class=\"btn-reset product__btn\" data-graph-path=\"prod-modal\" data-id=\"").concat(item.id, "\" aria-label=\"\u041F\u043E\u043A\u0430\u0437\u0430\u0442\u044C \u0438\u043D\u0444\u043E\u0440\u043C\u0430\u0446\u0438\u044E \u043E \u0442\u043E\u0432\u0430\u0440\u0435\">\n                        <svg>\n                          <use xlink:href=\"img/sprite.svg#eye\"></use>\n                        </svg>\n                      </button>\n                      <button class=\"btn-reset product__btn add-to-cart-btn\" data-id=\"").concat(item.id, "\" aria-label=\"\u0414\u043E\u0431\u0430\u0432\u0438\u0442\u044C \u0442\u043E\u0432\u0430\u0440 \u0432 \u043A\u043E\u0440\u0437\u0438\u043D\u0443\">\n                        <svg>\n                          <use xlink:href=\"img/sprite.svg#cart\"></use>\n                        </svg>\n                      </button>\n                    </div>\n                  </div>\n                  <h3 class=\"product__title\">").concat(item.title, "</h3>\n                  <span class=\"product__price\">").concat(normalPrice(item.price), " \u0440</span>\n                </article>\n              </li>\n            ");
+        }
+      }
+    }).then(function () {
+      var productTitle = document.querySelectorAll('.product__title');
+      productTitle.forEach(function (el) {
+        $clamp(el, {
+          clamp: '22px'
+        });
+      });
+      var productsBtns = document.querySelectorAll('.product__btn');
+      productsBtns.forEach(function (el) {
+        console.log(el);
+        el.addEventListener('focus', function (e) {
+          var parent = e.currentTarget.closest('.product__btns');
+          console.log(parent);
+          parent.classList.add('product__btns--active');
+        }, true);
+        el.addEventListener('blur', function (e) {
+          var parent = e.currentTarget.closest('.product__btns');
+          console.log(parent);
+          parent.classList.remove('product__btns--active');
+        }, true);
+      });
+      cartLogic();
+      modal = new GraphModal({
+        isOpen: function isOpen(modal) {
+          if (modal.modalContainer.classList.contains('prod-modal')) {
+            var openBtnId = modal.previousActiveElement.dataset.id;
+            loadModalData(openBtnId);
+            prodSlider.update();
+          }
+        }
+      });
+    });
+  };
+
+  loadProducts(prodQuantity);
+
+  var loadModalData = function loadModalData() {
+    var id = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 1;
+    fetch('../data/data.json').then(function (response) {
+      return response.json();
+    }).then(function (data) {
+      prodModalSlider.innerHTML = '';
+      prodModalPreview.innerHTML = '';
+      prodModalInfo.innerHTML = '';
+      prodModalDescr.textContent = '';
+      prodModalChars.innerHTML = '';
+      prodModalVideo.innerHTML = '';
+
+      var _iterator = _createForOfIteratorHelper(data),
+          _step;
+
+      try {
+        var _loop = function _loop() {
+          var dataItem = _step.value;
+
+          if (dataItem.id == id) {
+            console.log(dataItem);
+            var slides = dataItem.gallery.map(function (image, idx) {
+              return "\n                <div class=\"swiper-slide\" data-index=\"".concat(idx, "\">\n                  <img src=\"").concat(image, "\" alt=\"\">\n                </div>\n              ");
+            });
+            var preview = dataItem.gallery.map(function (image, idx) {
+              return "\n                <div class=\"modal-preview__item ".concat(idx === 0 ? 'modal-preview__item--active' : '', "\" tabindex=\"0\" data-index=\"").concat(idx, "\">\n                  <img src=\"").concat(image, "\" alt=\"\">\n                </div>\n              ");
+            });
+            var sizes = dataItem.sizes.map(function (size, idx) {
+              return "\n                <li class=\"modal-sizes__item\">\n                  <button class=\"btn-reset modal-sizes__btn\">".concat(size, "</button>\n                </li>\n              ");
+            });
+            prodModalSlider.innerHTML = slides.join('');
+            prodModalPreview.innerHTML = preview.join('');
+            prodModalInfo.innerHTML = "\n              <h3 class=\"modal-info__title\">".concat(dataItem.title, "</h3>\n              <div class=\"modal-info__rate\">\n                <img src=\"img/star.svg\" alt=\"\u0420\u0435\u0439\u0442\u0438\u043D\u0433 5 \u0438\u0437 5\">\n                <img src=\"img/star.svg\" alt=\"\">\n                <img src=\"img/star.svg\" alt=\"\">\n                <img src=\"img/star.svg\" alt=\"\">\n                <img src=\"img/star.svg\" alt=\"\">\n              </div>\n              <div class=\"modal-info__sizes\">\n                <span class=\"modal-info__subtitle\">\u0412\u044B\u0431\u0435\u0440\u0438\u0442\u0435 \u0440\u0430\u0437\u043C\u0435\u0440</span>\n                <ul class=\"list-reset modal-info__sizes-list modal-sizes\">\n                  ").concat(sizes.join(''), "\n                </ul>\n              </div>\n              <div class=\"modal-info__price\">\n                <span class=\"modal-info__current-price\">").concat(dataItem.price, " \u0440</span>\n                <span class=\"modal-info__old-price\">").concat(dataItem.oldPrice ? dataItem.oldPrice + 'р' : '', "</span>\n              </div>\n            ");
+            prodModalDescr.textContent = dataItem.description;
+            var charsItems = '';
+            Object.keys(dataItem.chars).forEach(function eachKey(key) {
+              charsItems += "<p class=\"prod-bottom__descr prod-chars__item\">".concat(key, ": ").concat(dataItem.chars[key], "</p>");
+            });
+            prodModalChars.innerHTML = charsItems;
+
+            if (dataItem.video) {
+              prodModalVideo.style.display = 'block';
+              prodModalVideo.innerHTML = "\n                <iframe src=\"".concat(dataItem.video, "\"\n                allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\"\n                allowfullscreen></iframe>\n              ");
+            } else {
+              prodModalVideo.style.display = 'none';
+            }
+          }
+        };
+
+        for (_iterator.s(); !(_step = _iterator.n()).done;) {
+          _loop();
+        }
+      } catch (err) {
+        _iterator.e(err);
+      } finally {
+        _iterator.f();
+      }
+    }).then(function () {
+      prodSlider.update();
+      prodSlider.on('slideChangeTransitionEnd', function () {
+        var idx = document.querySelector('.swiper-slide-active').dataset.index;
+        document.querySelectorAll('.modal-preview__item').forEach(function (el) {
+          el.classList.remove('modal-preview__item--active');
+        });
+        document.querySelector(".modal-preview__item[data-index=\"".concat(idx, "\"]")).classList.add('modal-preview__item--active');
+      });
+      document.querySelectorAll('.modal-preview__item').forEach(function (el) {
+        el.addEventListener('click', function (e) {
+          var idx = parseInt(e.currentTarget.dataset.index);
+          document.querySelectorAll('.modal-preview__item').forEach(function (el) {
+            el.classList.remove('modal-preview__item--active');
+          });
+          e.currentTarget.classList.add('modal-preview__item--active');
+          prodSlider.slideTo(idx);
+        });
+      });
+    });
+  };
+
+  catalogMore.addEventListener('click', function (e) {
+    prodQuantity = prodQuantity + 3;
+    loadProducts(prodQuantity);
+
+    if (prodQuantity >= dataLength) {
+      catalogMore.style.display = 'none';
+    } else {
+      catalogMore.style.display = 'block';
+    }
+  });
+} // работа корзины
+
+
+var price = 0;
+var miniCartList = document.querySelector('.mini-cart__list');
+var fullPrice = document.querySelector('.mini-cart__summ');
+var cartCount = document.querySelector('.cart__count');
+
+var priceWithoutSpaces = function priceWithoutSpaces(str) {
+  return str.replace(/\s/g, '');
+};
+
+var plusFullPrice = function plusFullPrice(currentPrice) {
+  return price += currentPrice;
+};
+
+var minusFullPrice = function minusFullPrice(currentPrice) {
+  return price -= currentPrice;
+};
+
+var printFullPrice = function printFullPrice() {
+  fullPrice.textContent = "".concat(normalPrice(price), " \u0440");
+};
+
+var printQuantity = function printQuantity(num) {
+  cartCount.textContent = num;
+};
+
+var loadCartData = function loadCartData() {
+  var id = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 1;
+  fetch('../data/data.json').then(function (response) {
+    return response.json();
+  }).then(function (data) {
+    var _iterator2 = _createForOfIteratorHelper(data),
+        _step2;
+
+    try {
+      for (_iterator2.s(); !(_step2 = _iterator2.n()).done;) {
+        var dataItem = _step2.value;
+
+        if (dataItem.id == id) {
+          console.log(dataItem);
+          miniCartList.insertAdjacentHTML('afterbegin', "\n              <li class=\"mini-cart__item\" data-id=\"".concat(dataItem.id, "\">\n                <article class=\"mini-cart__product mini-product\">\n                  <div class=\"mini-product__image\">\n                    <img src=\"").concat(dataItem.mainImage, "\" alt=\"").concat(dataItem.title, "\">\n                  </div>\n                  <div class=\"mini-product__content\">\n                    <div class=\"mini-product__text\">\n                      <h3 class=\"mini-product__title\">").concat(dataItem.title, "</h3>\n                      <span class=\"mini-product__price\">").concat(normalPrice(dataItem.price), " \u0440</span>\n                    </div>\n                    <button class=\"btn-reset mini-product__delete\" aria-label=\"\u0423\u0434\u0430\u043B\u0438\u0442\u044C \u0442\u043E\u0432\u0430\u0440\">\n                      \u0423\u0434\u0430\u043B\u0438\u0442\u044C\n                      <svg>\n                        <use xlink:href=\"img/sprite.svg#trash\"></use>\n                      </svg>\n                    </button>\n                  </div>\n                </article>\n              </li>\n            "));
+          return dataItem;
+        }
+      }
+    } catch (err) {
+      _iterator2.e(err);
+    } finally {
+      _iterator2.f();
+    }
+  }).then(function (item) {
+    plusFullPrice(item.price);
+    printFullPrice();
+    var num = document.querySelectorAll('.mini-cart__list .mini-cart__item').length;
+
+    if (num > 0) {
+      cartCount.classList.add('cart__count--visible');
+    }
+
+    printQuantity(num);
+  });
+};
+
+var cartLogic = function cartLogic() {
+  var productBtn = document.querySelectorAll('.add-to-cart-btn');
+  productBtn.forEach(function (el) {
+    el.addEventListener('click', function (e) {
+      var id = e.currentTarget.dataset.id;
+      loadCartData(id);
+      document.querySelector('.cart__btn').classList.remove('cart__btn--inactive');
+      e.currentTarget.classList.add('product__btn--disabled');
+    });
+  });
+  miniCartList.addEventListener('click', function (e) {
+    if (e.target.classList.contains('mini-product__delete')) {
+      var self = e.target;
+      var parent = self.closest('.mini-cart__item');
+
+      var _price = parseInt(priceWithoutSpaces(parent.querySelector('.mini-product__price').textContent));
+
+      var id = parent.dataset.id;
+      console.log(document.querySelector(".product__btn[data-id=\"".concat(id, "\"]")));
+      document.querySelector(".add-to-cart-btn[data-id=\"".concat(id, "\"]")).classList.remove('product__btn--disabled');
+      parent.remove();
+      minusFullPrice(_price);
+      printFullPrice();
+      var num = document.querySelectorAll('.mini-cart__list .mini-cart__item').length;
+
+      if (num == 0) {
+        cartCount.classList.remove('cart__count--visible');
+        miniCart.classList.remove('mini-cart--visible');
+        document.querySelector('.cart__btn').classList.add('cart__btn--inactive');
+      }
+
+      printQuantity(num);
+    }
+  });
+};
+
+var openOrderModal = document.querySelector('.mini-cart__btn');
+var orderModalList = document.querySelector('.cart-modal-order__list');
+var orderModalQuantity = document.querySelector('.cart-modal-order__quantity span');
+var orderModalSumm = document.querySelector('.cart-modal-order__summ span');
+var orderModalShow = document.querySelector('.cart-modal-order__show');
+openOrderModal.addEventListener('click', function () {
+  var productsHtml = document.querySelector('.mini-cart__list').innerHTML;
+  orderModalList.innerHTML = productsHtml;
+  orderModalQuantity.textContent = "".concat(document.querySelectorAll('.mini-cart__list .mini-cart__item').length, " \u0448\u0442");
+  orderModalSumm.textContent = fullPrice.textContent;
+});
+orderModalShow.addEventListener('click', function () {
+  if (orderModalList.classList.contains('cart-modal-order__list--visible')) {
+    orderModalList.classList.remove('cart-modal-order__list--visible');
+    orderModalShow.classList.remove('cart-modal-order__show--active');
+  } else {
+    orderModalList.classList.add('cart-modal-order__list--visible');
+    orderModalShow.classList.add('cart-modal-order__show--active');
+  }
+}); // ДОДЕЛАТЬ УДАЛЕНИЕ ИЗ МОДАЛКИ
+
+orderModalList.addEventListener('click', function (e) {
+  if (e.target.classList.contains('mini-product__delete')) {
+    console.log('asd');
+    var self = e.target;
+    var parent = self.closest('.mini-cart__item');
+
+    var _price2 = parseInt(priceWithoutSpaces(parent.querySelector('.mini-product__price').textContent));
+
+    var id = parent.dataset.id;
+    console.log(document.querySelector(".product__btn[data-id=\"".concat(id, "\"]")));
+    document.querySelector(".add-to-cart-btn[data-id=\"".concat(id, "\"]")).classList.remove('product__btn--disabled');
+    parent.style.display = 'none';
+    setTimeout(function () {
+      parent.remove();
+    }, 100);
+    document.querySelector(".mini-cart__item[data-id=\"".concat(id, "\"]")).remove();
+    minusFullPrice(_price2);
+    printFullPrice();
+    setTimeout(function () {
+      var num = document.querySelectorAll('.cart-modal-order__list .mini-cart__item').length;
+      console.log(num);
+
+      if (num == 0) {
+        cartCount.classList.remove('cart__count--visible');
+        miniCart.classList.remove('mini-cart--visible');
+        document.querySelector('.cart__btn').classList.add('cart__btn--inactive');
+        modal.close();
+      }
+
+      printQuantity(num);
+    }, 100);
+  }
+});
+"use strict";
+
 function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
 function _createForOfIteratorHelper(o, allowArrayLike) { var it = typeof Symbol !== "undefined" && o[Symbol.iterator] || o["@@iterator"]; if (!it) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e) { throw _e; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = it.call(o); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e2) { didErr = true; err = _e2; }, f: function f() { try { if (!normalCompletion && it["return"] != null) it["return"](); } finally { if (didErr) throw err; } } }; }
